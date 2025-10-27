5c97580d6d1acf9c8aaec5bea9d9747637c96e63
kinhenrik <hkin98@gmail.com>
2025-10-27 12:20:04 +0100

    Initial commit

---
+++ src/main/java/com/comp/edufymediaplayerservice/EdufyMediaplayerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.comp.edufymediaplayerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyMediaplayerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdufyMediaplayerServiceApplication.class, args);
+    }
+
+}
