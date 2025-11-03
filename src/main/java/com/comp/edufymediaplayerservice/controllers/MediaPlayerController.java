c006ae60a97735cb0f4a9f8c0b78dd9db4ed752e
samuel.funck <159692435+samuelFNK@users.noreply.github.com>
2025-11-03 11:10:11 +0100

    *media player controller with basic endpoints. No connections to other classes or dependencies yet

--- src/main/java/com/comp/edufymediaplayerservice/controllers/MediaPlayerController.java
+++ src/main/java/com/comp/edufymediaplayerservice/controllers/MediaPlayerController.java
@@ -1,5 +1,34 @@
 package com.comp.edufymediaplayerservice.controllers;
 
-public class MediaPlayerController {
-    
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("edufy/api/mediaplayer/")
+public class MediaplayerController {
+
+    private final MediaplayerService mediaplayerService;
+
+    public MediaplayerController(MediaplayerService mediaplayerService){
+        this.mediaplayerService = mediaplayerService;
+    }
+
+    @GetMapping("media/{mediaName}")
+    public Object getMediaByName(@PathVariable String mediaName){
+        return mediaplayerService.getMediaByName(mediaName);
+    }
+
+    @GetMapping("album/{albumName}")
+    public Object getAlbumByName(@PathVariable String albumName){
+        return mediaplayerService.getAlbumByName(albumName);
+    }
+
+    @GetMapping("artist/{artistName}")
+    public Object getArtistByName(@PathVariable String artistName){
+        return mediaplayerService.getArtistByName(artistName);
+
+    }
+
 }
